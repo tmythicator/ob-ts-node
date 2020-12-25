19a25ac1bb16e6eae4a5b925ad5af5180c8ac4f0
Alexandr Timchenko <atimchenko92@gmail.com>
2020-12-25 12:00:21 +0100

    Changed license to GPLv3. Updated description

--- ob-ts-node.el
+++ ob-ts-node.el
@@ -1,4 +1,4 @@
-;;; ob-ts-node.el --- org-babel functions for Typescript evaluation with ts-node.
+;;; ob-ts-node.el --- org-babel support for evaluating typescript code.
 
 ;; Copyright (C) 2020 Alexandr Timchenko
 
@@ -10,31 +10,24 @@
 
 ;;; License:
 
-;; The MIT License (MIT)
-
-;; Copyright (c) 2020 Alexandr Timchenko
-
-;; Permission is hereby granted, free of charge, to any person obtaining a copy
-;; of this software and associated documentation files (the "Software"), to deal
-;; in the Software without restriction, including without limitation the rights
-;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-;; copies of the Software, and to permit persons to whom the Software is
-;; furnished to do so, subject to the following conditions:
-
-;; The above copyright notice and this permission notice shall be included in
-;; all copies or substantial portions of the Software.
-
-;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-;; THE SOFTWARE.
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
 
-;; Org-babel support for evaluating typescript files, based on support of ts-node.
+;; Org-babel support for evaluating typescript code, based on support of ts-node.
 ;; See: https://github.com/TypeStrong/ts-node
 
 ;;; Requirements:
